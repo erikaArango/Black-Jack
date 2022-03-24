f423d6570528801f7f5f51d72f67d3584df873fa
erikaArango <erika.arango@walmart.com>
2022-03-23 21:38:47 -0300

    order card

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,7 +1,7 @@
 let deck = [];
 const types = ['C','D','H','S'];
 const especialCards = ['A','J','Q','K'];
-
+//this function creates a new deck
 const createDeck = () => {
     for (let i = 2; i <=10; i++ ){
         for(let type of types ) {
@@ -14,12 +14,25 @@ const createDeck = () => {
         deck.push( esp + type);
         }
     }
-    console.log(deck);
-
+    //console.log(deck);
     deck = _.shuffle(deck);
     console.log('shuffled array',deck);
 
     return deck;
 }
-
 createDeck();
+
+//his function allows me to take a letter
+const orderCard = () => {
+    if (deck.length === 0 ){
+        throw 'No hay Cartas en el deck';
+    }
+    const card = deck.pop();
+
+    console.log(deck);
+    console.log(card);
+    return card;
+}
+for ( let i = 0; i <= 100; i++){
+orderCard();
+}
